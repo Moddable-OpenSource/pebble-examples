57785bef351f563d448782c903e5073d4ddec36d
Peter Hoddie <peter@moddable.com>
2025-08-18 19:46:57 -0700

    remove more rocky

--- hellopoco-gbitmap/src/embeddedjs/main.js
+++ hellopoco-gbitmap/src/embeddedjs/main.js
@@ -12,12 +12,8 @@ let index = 0;
 setInterval(() => {
 	index = (index + 1) % 4;
 	bitmap = new Poco.PebbleBitmap(2 + index);
-	rocky.requestDraw();
-}, 250);
-
- rocky.on('draw', function() {
 	render.begin();
 	render.fillRectangle(black, 0, 0, render.width, render.height);	
 	render.drawBitmap(bitmap, (render.width - bitmap.width) / 2, (render.height - bitmap.height) / 2);
 	render.end();
-});
+}, 250);
