832a1e8ca070f51bd488838fb359b352a926404c
Patrick Soquet <ps@moddable.tech>
2025-08-21 16:16:35 +0200

    piu watchfaces

---
+++ piu/watchfaces/cupertino/src/embeddedjs/main.js
@@ -0,0 +1,76 @@
+const dialSkin = new Skin({ texture: new Texture(`dial.png`), width:144, height:168 });
+
+class FaceApplicationBehavior {
+	onCreate(application, $) {
+		this.clock = {};
+	}
+	onDisplaying(application) {
+		this.onTimeChanged(application);
+		application.interval = 500;
+		application.start();
+	}
+	onTimeChanged(application) {
+		const date = new Date();
+		this.clock.year = date.getFullYear();
+		this.clock.month = date.getMonth();
+		this.clock.date = date.getDate();
+		this.clock.day = date.getDay();
+		this.clock.hours = date.getHours();
+		this.clock.minutes = date.getMinutes();
+		application.distribute("onClockChanged", this.clock);
+	}
+}
+
+class FaceHandBehavior {
+	onIndexChanged(content, index) {
+		const data = this.data;
+		index *= 6;
+		const x = data[index];
+		const y = data[index+1];
+		const sw = data[index+2];
+		const sh = data[index+3];
+		const sx = data[index+4];
+		const sy = data[index+5];
+		content.skin = new Skin({ texture:this.texture, x:sx, y:sy, width:sw, height:sh, color:"black" });
+		const container = content.container;
+		content.x = container.x + (container.width >> 1) - x;
+		content.y = container.y + (container.height >> 1) - y;
+		content.width = sw;
+		content.height = sh;
+	}
+}
+
+class FaceHoursBehavior extends FaceHandBehavior {
+	onCreate(content) {
+		this.data = new Int16Array(new Resource("hours.data"));
+		this.texture = new Texture(`hours.png`);
+	}
+	onClockChanged(content, clock) {
+		this.onIndexChanged(content, ((clock.hours % 12) * 5) + Math.floor(clock.minutes / 12));
+	}
+}
+
+class FaceMinutesBehavior extends FaceHandBehavior {
+	onCreate(content) {
+		this.data = new Int16Array(new Resource("minutes.data"));
+		this.texture = new Texture(`minutes.png`);
+	}
+	onClockChanged(content, clock) {
+		this.onIndexChanged(content, clock.minutes);
+	}
+}
+
+const FaceApplication = Application.template($ => ({
+	left:0, right:0, top:0, bottom:0, Behavior:FaceApplicationBehavior,
+	contents: [
+		Container($, { 
+			skin: dialSkin,
+			contents: [
+				Content($, { Behavior: FaceHoursBehavior, left:0, width:144, top:0, height:168 }),
+				Content($, { Behavior: FaceMinutesBehavior, left:0, width:144, top:0, height:168 }),
+			]
+		}),
+	]
+}));
+
+export default new FaceApplication(null, { displayListLength:2048, touchCount:0, pixels: screen.width * 4,  });
---
+++ piu/watchfaces/helsinki/src/embeddedjs/main.js
@@ -0,0 +1,51 @@
+const blackSkin = new Skin({ fill:"black" });
+const digitsSkin = new Skin({ texture: new Texture(`digits.png`), width:24, height:40, variants:24 });
+const dateStyle = new Style({ font:"bold 14px Gothic", color:"black" });
+const days = Object.freeze([ "SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT" ]); 
+const whiteSkin = new Skin({ fill:"white" });
+
+class FaceApplicationBehavior {
+	onCreate(application, $) {
+		this.clock = {};
+		this.$ = $;
+	}
+	onDisplaying(application) {
+		this.onTimeChanged(application);
+		application.interval = 500;
+		application.start();
+	}
+	onTimeChanged(application) {
+		const date = new Date();
+		const hours = date.getHours();
+		const minutes = date.getMinutes();
+		let content = application.first;
+		content.variant = Math.floor(hours / 10);
+		content = content.next;
+		content.variant = hours % 10;
+		content = content.next;
+		content.visible = !content.visible;
+		content = content.next;
+		content.variant = Math.floor(minutes / 10);
+		content = content.next;
+		content.variant = minutes % 10;
+		content = content.next;
+		content.string = days[date.getDay()];
+		content = content.next;
+		content.string = date.toISOString().slice(0, 10);
+	}
+}
+
+const FaceApplication = Application.template($ => ({
+	Behavior:FaceApplicationBehavior, skin:blackSkin,
+	contents: [
+		Content($, { left:6, skin:digitsSkin }),
+		Content($, { left:36, skin:digitsSkin }),
+		Content($, { left:60, skin:digitsSkin, variant:10 }),
+		Content($, { left:84, skin:digitsSkin }),
+		Content($, { left:114, skin:digitsSkin }),
+		Label($, { left:0, right:0, top:0, skin:whiteSkin, style:dateStyle, string:"WEDNESDAY" }),
+		Label($, { left:0, right:0, bottom:0, skin:whiteSkin, style:dateStyle, string:"2025-06-11" }),
+	]
+}));
+
+export default new FaceApplication({}, { displayListLength:2048, touchCount:0, pixels: screen.width * 4,  });
