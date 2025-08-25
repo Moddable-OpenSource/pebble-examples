ae4b92ee0580d86cbc1966ddc599b889adba6ff5
Griffin Li <griffin@griffinli.com>
2025-08-25 13:35:27 -0700

    Delete setup.sh

--- hellofetch/setup.sh
+++
@@ -1,8 +0,0 @@
-set -e
-mkdir -p build
-mcrun -m ./src/embeddedjs/manifest.json -f x -t build -o ./build -s tech.moddable.fetch
-mkdir -p resources/mods
-cp build/bin/mac/mc/release/embeddedjs/mc.xsa ./resources/mods/mc.xsa
-rebble build
-rebble install --qemu localhost:12344
-pypkjs ./build/hellofetch.pbw  --debug
