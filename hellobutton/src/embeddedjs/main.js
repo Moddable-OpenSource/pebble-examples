0485c068033a2e06ea0e096d20f3d246f36dbb25
Peter Hoddie <peter@moddable.com>
2025-09-08 17:58:31 -0700

    use updated Button API

--- hellobutton/src/embeddedjs/main.js
+++ hellobutton/src/embeddedjs/main.js
@@ -3,11 +3,9 @@ import Button from "pebble/button"
 console.log("hello, button - press a button");
 console.log("  press and hold back to exit");
 
-["select", "up", "down", "back"].forEach(type => {
-	new Button({
-		type,
-		onPush(down) {
-			console.log(`${down ? "press" : "release"} ${type}`);
-		}
-	});
+new Button({
+	types: ["select", "up", "down", "back"],
+	onPush(down, type) {
+		console.log(`${down ? "press" : "release"} ${type}`);
+	}
 });
