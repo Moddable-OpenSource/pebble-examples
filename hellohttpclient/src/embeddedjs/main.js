ec1e55c5c17331a0f7b0c32e666d4b938e326184
Peter Hoddie <peter@moddable.com>
2026-01-20 15:28:55 -0800

    remove setTimeout to wait for PKJS to be ready

--- hellohttpclient/src/embeddedjs/main.js
+++ hellohttpclient/src/embeddedjs/main.js
@@ -1,33 +1,31 @@
 import HTTPClient from "embedded:network/http/client";
 
-setTimeout(() => {
-	const http = new HTTPClient({
-		host: "example.com"
-	});
+const http = new HTTPClient({
+	host: "example.com"
+});
 
-	http.request({
-		path: "/",
-		headersMask: ["date"],
-		headers: new Map([
-			["date", (new Date).toString()],
-			["user-agent", "pebble test"]
-		]),
-		onHeaders(status, headers, statusText) {
-			trace(`Status ${status}: ${statusText}\n`);
-			headers.forEach((value, key) => {
-				trace(`${key}: ${value}\n`);
-			});
-		},
-		onReadable(count) {
-			try {
-				for (let offset = 0, step = 80; offset < count; offset += step) {
-					const buffer = this.read(step);
-					trace(String.fromArrayBuffer(buffer));
-				}
-			}
-			catch (e) {
-				trace("read error: " + e);
+http.request({
+	path: "/",
+	headersMask: ["date"],
+	headers: new Map([
+		["date", (new Date).toString()],
+		["user-agent", "pebble test"]
+	]),
+	onHeaders(status, headers, statusText) {
+		trace(`Status ${status}: ${statusText}\n`);
+		headers.forEach((value, key) => {
+			trace(`${key}: ${value}\n`);
+		});
+	},
+	onReadable(count) {
+		try {
+			for (let offset = 0, step = 80; offset < count; offset += step) {
+				const buffer = this.read(step);
+				trace(String.fromArrayBuffer(buffer));
 			}
 		}
-	});
-}, 1000);
+		catch (e) {
+			trace("read error: " + e);
+		}
+	}
+});
