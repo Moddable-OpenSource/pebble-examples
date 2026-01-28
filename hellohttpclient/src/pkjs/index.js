624292e910981dec3cb4be9ec43d9637994a2135
Peter Hoddie <peter@moddable.com>
2026-01-27 19:59:41 -0800

    appmessage - v2

--- hellohttpclient/src/pkjs/index.js
+++ hellohttpclient/src/pkjs/index.js
@@ -1,6 +1,7 @@
 const moddableProxy = require("@moddable/pebbleproxy");
+Pebble.addEventListener('ready', moddableProxy.readyReceived)
 Pebble.addEventListener('appmessage', function (e) {
-	if (moddableProxy.eventReceived(e))
+	if (moddableProxy.appMessageReceived(e))
 		return;
 
 	// This is not a Moddable proxy event. Handle the event here. 
