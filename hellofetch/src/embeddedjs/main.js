a8155324c476178a54b20443c309e683166cded7
Peter Hoddie <peter@moddable.com>
2026-02-18 14:25:45 -0800

    remove not-so-secret key

--- hellofetch/src/embeddedjs/main.js
+++ hellofetch/src/embeddedjs/main.js
@@ -1,4 +1,4 @@
-const appid = '81956bdbadae682541fc2ec70347de5b';
+const appid = 'YOUR_API_KEY';
 
 const url = new URL("http://api.openweathermap.org/data/2.5/weather");
 url.search = new URLSearchParams({
